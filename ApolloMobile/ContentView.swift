13d20b399b1b61dc87b4c30d66d1cfca0370a182
ShihTing <s-huang@unext.jp>
2020-03-15 17:02:40 +0900

    Initial Commit

---
+++ ApolloMobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApolloMobile
+//
+//  Created by s-huang on 2020/03/15.
+//  Copyright © 2020 Neo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
